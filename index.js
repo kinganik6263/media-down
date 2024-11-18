899c3a624c130915eb8181a5a3ec3b2b904c8527
kinganik6263 <161469632+kinganik6263@users.noreply.github.com>
2024-11-18 17:22:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require('./down');
 
 app.listen(3001, () => {
-    console.log("your server is running ✅")
+    console.log("your server is running ✅");
+    console.log("youtube, tiktok, Facebook, instagram")
 })
 
 
